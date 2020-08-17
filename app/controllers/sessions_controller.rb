96a6b03e847f7b3ff24dabbdbd86089cdd83f7de
Yukiko Noda <yukiko8318@gmail.com>
2020-08-17 16:29:21 +0900

    いいね機能のためにハートについてのCSS追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_path, success: 'ログインに成功しました'
+      # redirect_to root_path, success: 'ログインに成功しました'
+      redirect_to topics_path, success: 'ログインに成功しました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url, info: 'ログアウトしました'
   end
 
   private
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,6 +5,7 @@ class Topic < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
+  belongs_to :user
   has_many :favorites
   has_many :favorite_users, through: :favorites, source: 'user'
 end
